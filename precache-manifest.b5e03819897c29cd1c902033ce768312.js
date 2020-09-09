7401cb0337e2e6059a3e3d1610056edb7ddad870
Sandeep325 <negisandeep9819@gmail.com>
2020-09-09 21:33:18 +0530

    Updates

---
+++ precache-manifest.b5e03819897c29cd1c902033ce768312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e213d45524f714a7bd0ad67ec8f742",
+    "url": "/updatedigitaltime/index.html"
+  },
+  {
+    "revision": "2f1dd7a2bf677d2711bb",
+    "url": "/updatedigitaltime/static/css/main.d1fbad46.chunk.css"
+  },
+  {
+    "revision": "a0d0fa00a3ab5641cd4d",
+    "url": "/updatedigitaltime/static/js/2.20db5985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/updatedigitaltime/static/js/2.20db5985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1dd7a2bf677d2711bb",
+    "url": "/updatedigitaltime/static/js/main.d373117e.chunk.js"
+  },
+  {
+    "revision": "32770049d2ef7b2b7ed9",
+    "url": "/updatedigitaltime/static/js/runtime-main.8d558422.js"
+  }
+]);
